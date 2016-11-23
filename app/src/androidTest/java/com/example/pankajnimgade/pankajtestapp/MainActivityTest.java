f07702d02f499fa79814267b7ce5b70ba6943882
pankaj-nimgade-globallogic <pankaj.nimgade@globallogic.com>
2016-11-23 12:43:53 +0530

    added wrapper task and files related to it

---
+++ app/src/androidTest/java/com/example/pankajnimgade/pankajtestapp/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.pankajnimgade.pankajtestapp;
+
+import android.support.test.runner.AndroidJUnitRunner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pankaj.nimgade on 22-11-2016.
+ */
+public class MainActivityTest extends AndroidJUnitRunner {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/com/example/pankajnimgade/pankajtestapp/MyItemTest.java
@@ -0,0 +1,23 @@
+package com.example.pankajnimgade.pankajtestapp;
+
+import android.test.AndroidTestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+
+/**
+ * Created by pankaj.nimgade on 22-11-2016.
+ */
+public class MyItemTest extends AndroidTestCase {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
